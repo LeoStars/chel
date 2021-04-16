e991fa56181b03a2f20723239581489c20819cb7
LeoStars <lightsn97@mail.ru>
2021-04-16 10:45:27 +0300

    fourth

--- chel/app/src/main/java/ru/iu3/chel/MainActivity.java
+++ chel/app/src/main/java/ru/iu3/chel/MainActivity.java
@@ -9,10 +9,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.btnClickMe);
         btn.setOnClickListener((View v) -> { onButtonClick(v);});
+
+        Button btnHttp = findViewById(R.id.btnHttp);
+        btnHttp.setOnClickListener((View v) -> {
+            onButtonHttpClick(v);
+        });
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
         // Button btn = findViewById(R.id.btnClickMe);
         // btn.setOnClickListener((View v) -> { onButtonClick(v);});
@@ -94,6 +105,49 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, PinpadActivity.class);
        // startActivity(it);
 
-         startActivityForResult(it, 0);
+        startActivityForResult(it, 0);
+    }
+
+    protected void onButtonHttpClick(View v) {
+        TestHttpClient();
+    }
+
+    protected void TestHttpClient() {
+        new Thread(() -> {
+            try {
+                //HttpURLConnection uc = (HttpURLConnection) (new URL("https://www.wikipedia.org").openConnection());
+               HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() -> {
+                    Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+
+    protected String getPageTitle(String html) {
+        /*int pos = html.indexOf("<title");
+        String p = "not found";
+        if (pos >= 0)
+        {
+            int pos2 = html.indexOf("<", pos + 1);
+            if (pos >= 0)
+                p = html.substring(pos + 7, pos2);
+        }
+        return p;*/
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find()) {
+            p = matcher.group(1);
+        } else {
+            p = "Not Found";
+        }
+        return p;
     }
 }
